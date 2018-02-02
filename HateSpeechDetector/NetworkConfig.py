2554cd2093ea55dcce4f9a8a3620fe3d5a655f13
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2018-02-02 11:40:10 +0100

    Load and save configs finished

--- HateSpeechDetector/NetworkConfig.py
+++ HateSpeechDetector/NetworkConfig.py
@@ -1,9 +1,11 @@
 class NetworkConfig:
 
-    def read_config_from_file(self, filename):
+    def read_config_from_file(self, filename):  # Should probably change representation to dictionary
         try:
             with open(filename, 'r') as f:
-                self.learning_rate = int(f.readline().split()[1])
+                self.learning_rate = float(f.readline().split()[1])
+                self.bidirectional = (f.readline().split()[1] == 'True')
+                self.dropout_keep_prob = float(f.readline().split()[1])
                 self.batch_size = int(f.readline().split()[1])
                 num_conv_filters = int(f.readline())
                 self.conv_filters_per_layer = []
@@ -13,6 +15,12 @@ class NetworkConfig:
                 for i in range(num_conv_filters):
                     self.conv_filter_lengths.append(int(f.readline()))
                 self.num_char_lstm_layers = int(f.readline().split()[1])
+                self.num_word_lstm_layers = int(f.readline().split()[1])
+                self.word_lstm_layer_size = int(f.readline().split()[1])
+                num_dense_layers = int(f.readline())
+                self.dense_layer_sizes = []
+                for i in range(num_dense_layers):
+                    self.dense_layer_sizes.append(int(f.readline()))
         except IOError:
             print('No such config file: {}'.format(filename))
 
@@ -20,15 +28,34 @@ class NetworkConfig:
         try:
             with open(filename, 'w') as f:
                 f.write('learning_rate: {}\n'.format(self.learning_rate))
+                f.write('bidirectional: {}\n'.format(self.bidirectional))
+                f.write('dropout_keep_prob: {}\n'.format(self.dropout_keep_prob))
                 f.write('batch_size: {}\n'.format(self.batch_size))
-                num_conv_filters = len(self.conv_filter_lengths)
-                f.write('{}\n'.format(num_conv_filters))
-                for i in range(num_conv_filters):
-                    f.write('{}\n'.format(self.conv_filters_per_layer[i]))
-                for i in range(num_conv_filters):
-                    f.write('{}\n'.format(self.conv_filter_lengths[i]))
+                f.write('{}\n'.format(len(self.conv_filter_lengths)))
+                for filters in self.conv_filters_per_layer:
+                    f.write('{}\n'.format(filters))
+                for length in self.conv_filter_lengths:
+                    f.write('{}\n'.format(length))
                 f.write('num_char_lstm_layers: {}\n'.format(self.num_char_lstm_layers))
+                f.write('num_word_lstm_layers: {}\n'.format(self.num_word_lstm_layers))
+                f.write('word_lstm_layer_size: {}\n'.format(self.word_lstm_layer_size))
+                f.write('{}\n'.format(len(self.dense_layer_sizes)))
+                for layer in self.dense_layer_sizes:
+                    f.write('{}\n'.format(layer))
         except IOError:
             with open(filename, 'x') as f:
                 f.write('learning_rate: {}\n'.format(self.learning_rate))
+                f.write('bidirectional: {}\n'.format(self.bidirectional))
+                f.write('dropout_keep_prob: {}\n'.format(self.dropout_keep_prob))
                 f.write('batch_size: {}\n'.format(self.batch_size))
+                f.write('{}\n'.format(len(self.conv_filter_lengths)))
+                for filters in self.conv_filters_per_layer:
+                    f.write('{}\n'.format(filters))
+                for length in self.conv_filter_lengths:
+                    f.write('{}\n'.format(length))
+                f.write('num_char_lstm_layers: {}\n'.format(self.num_char_lstm_layers))
+                f.write('num_word_lstm_layers: {}\n'.format(self.num_word_lstm_layers))
+                f.write('word_lstm_layer_size: {}\n'.format(self.word_lstm_layer_size))
+                f.write('{}\n'.format(len(self.dense_layer_sizes)))
+                for layer in self.dense_layer_sizes:
+                    f.write('{}\n'.format(layer))
