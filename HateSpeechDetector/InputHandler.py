0ad8dac275cb943596ee94464745b88083125fe0
Johannes Skjeggestad Meyer <johannes@skmeyer.com>
2018-02-22 13:59:03 +0100

    Split hashtags instead of remove; support for unknown terms

--- HateSpeechDetector/InputHandler.py
+++ HateSpeechDetector/InputHandler.py
@@ -1,14 +1,17 @@
 from gensim.models import KeyedVectors
+import numpy as np
 import os
 import preprocessor as p
-import tensorflow as tf
 import re
+import Utils
 
 
 alphabet = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12,
             'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,
             'z': 25, ' ': 26, '#': 27, '@': 28, '0': 29, '1': 29, '2': 29, '3': 29, '4': 29, '5': 29, '6': 29, '7': 29,
             '8': 29, '9': 29}
+
+unknown_vector = np.random.uniform(-.5, .5, 300)
 
 
 def generate_word2vec_model():
@@ -20,31 +23,52 @@ def generate_word2vec_model():
 
 def preprocess_character_based(tweets):
     p.set_options(p.OPT.EMOJI)  # Include urls?
-    indx = []
+    batch = []
     for tweet in tweets:
         clean = p.clean(tweet)
         clean = clean.lower()
         chars = []
         for ch in clean:
             if ch in alphabet:
-                chars.append(alphabet[ch])
+                i = alphabet[ch]
             else:
-                chars.append(30)
-        indx.append(chars)
-    batch = tf.one_hot(indx, 31)
+                i = 30
+            chars.append(Utils.int_to_one_hot(i, 31))
+        batch.append(chars)
     return batch
 
 
 def preprocess_word_based(tweets, vocab_model):
-    p.set_options(p.OPT.EMOJI, p.OPT.URL, p.OPT.MENTION, p.OPT.HASHTAG)
+    p.set_options(p.OPT.EMOJI, p.OPT.URL, p.OPT.MENTION)  # , p.OPT.HASHTAG)
     batch = []
     pattern = re.compile('[^0-9a-z\s]+', re.UNICODE)
     for tweet in tweets:
         # Should I remove hashtags completely, or just remove the symbol?
         clean = p.clean(tweet)
+        clean = split_hashtags(clean)
         clean = clean.lower()
         clean = pattern.sub('', clean)
         words = clean.split()
-        res = [vocab_model.word_vec(word) for word in words]
+        res = []
+        for word in words:
+            if word != '':
+                try:
+                    vec = vocab_model.word_vec(word)
+                except KeyError:
+                    vec = unknown_vector
+                finally:
+                    res.append(vec)
         batch.append(res)
     return batch
+
+
+def split_hashtags(tweet):
+    terms = tweet.split()
+    for i, word in enumerate(terms):
+        if word.startswith('#'):
+            word = word[1:]
+            if word.upper() != word:
+                word = re.split('[A-Z]', word)
+                word = ' '.join(word)
+            terms[i] = word
+    return ' '.join(terms)
