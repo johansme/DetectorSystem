376aa6cf76ce68d6c915a6aa110cadb8e0d0d864
Johannes Meyer <johannes@skmeyer.com>
2018-02-23 13:02:53 +0100

    Implemented 10-fold cross validation

--- HateSpeechDetector/Detector.py
+++ HateSpeechDetector/Detector.py
@@ -19,8 +19,43 @@ def test_classifier():
             labels[i] = Utils.int_to_one_hot(lab, 3)
         full_data.append([char_input, word_input, labels])
     word2vec = None
-    classifier.set_data(full_data[:-2], full_data[-2:])
-    classifier.do_training(full_data[:-2], 21)
+    classifier.set_data(training_data=full_data[:-2], validation_data=full_data[-2:])
+    classifier.do_training(21)
 
 
-test_classifier()
+def do_10fold_cross_validation(config_name):
+    classifier = HateSpeechClassifier.ClassifierNetwork(config_name, 20)
+    folds = []
+    for i in range(1, 11):
+        fold = Utils.read_tweets_from_folds(i)
+        fold = Utils.create_batches(fold, classifier.batch_size)
+        folds.append(fold)
+    word2vec = InputHandler.generate_word2vec_model()
+    for i, fold in enumerate(folds):
+        fold_batches = []
+        for batch in fold:
+            data, labels = Utils.separate_data_from_labels(batch)
+            word_input = InputHandler.preprocess_word_based(data, word2vec)
+            word_input = Utils.batch_to_3d_array(word_input)
+            char_input = InputHandler.preprocess_character_based(data)
+            char_input = Utils.batch_to_3d_array(char_input)
+            for j, lab in enumerate(labels):
+                labels[j] = Utils.int_to_one_hot(lab, 3)
+            fold_batches.append([char_input, word_input, labels])
+        folds[i] = fold_batches
+    for i in range(10):
+        val_fold = folds[i]
+        train_folds = folds[:i] + (folds[i+1:] if i < 9 else [])
+        run_cross_validation_round(train_folds, val_fold, classifier)
+
+
+def run_cross_validation_round(training_folds, validation_fold, classifier):
+    training_data = []
+    for fold in training_folds:
+        training_data.extend(fold)
+    classifier.set_data(training_data=training_data, validation_data=validation_fold)
+    classifier.do_training(1000)
+    classifier.do_testing(test_data=validation_fold)
+
+
+do_10fold_cross_validation('TestConfig.txt')
